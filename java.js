19388b5b8a612674a101a595c3d58ed8138834c0
seyma <symbglr54@gmail.com>
2023-05-22 19:13:20 +0300

    first commit

---
+++ java.js
@@ -0,0 +1,16 @@
+const button = document.querySelector("div");
+const body = document.querySelector("body");
+const audio = document.querySelector("audio");
+// const color = ["#fff","purple","tomate" ,"green", "gray", "black", "orange", "brown"]
+
+button.addEventListener("click", result)
+
+function result(){
+    // const index = Math.round(Math.random()*10)
+    audio.play();
+    // const index = Math.round(Math.random()*color.length)
+    // body.style.backgroundColor =color[index];
+    // button.style.color=color[index-1]
+    // button.style.backgroundColor=color[index-2]
+
+}
